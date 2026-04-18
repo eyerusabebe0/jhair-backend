c53ba4e733f7b4cf1a319c3a6d044598cab25a11
Jerry <eyerusabebe0@gmail.com>
2026-04-18 03:43:59 -0700

    Fix cors in server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const adminRoute = require("./routes/adminRoute");
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://hair-e-commerce.vercel.app'],
+  origin: 'https://jhair-frontend-4kne.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
